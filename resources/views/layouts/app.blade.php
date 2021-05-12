434f7afa931e960e670faeaf0026ea896b4e062f
axtx4869 <shota1445@icloud.com>
2021-05-12 22:38:17 +0900

    ブラウザのタブにログインユーザ名を表示しないようにするため削除

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }} @yield('title')</title>
+    <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/main.js') }}" defer></script>
